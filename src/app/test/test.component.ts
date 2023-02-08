f3a412326cce773582925dc656e1f642b50ea13a
Wasif Zaman <75499379+wasifzaman182@users.noreply.github.com>
2023-02-08 23:28:11 +0500

    Cover multiple topics
    such as
    data binding
    event binding
    two way binding
    template reference variable

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -5,7 +5,7 @@ import { Component, OnInit } from '@angular/core';
   template: `<h2>
       Examples of data binding by using interpolation method
   </h2>
-  <br><br>
+  <br>
   <h3>
     Hello dynamic {{username}} use
   </h3>
@@ -35,15 +35,72 @@ import { Component, OnInit } from '@angular/core';
     Example of Property binding using bind-propertyName
   </h3>
   <input bind-disabled="isDisabled" type="text" value="wasif">
+  <br><br>
+  <h3 class="text-success">
+    Example of class binding Example 1
+  </h3>
+  <br>
+  <h3 [class]="successClass">
+    Example of class binding Example by defining proprty 
+  </h3>
+  <br>
+  <h3  class="text-special" [class]="successClass">
+    Example of class binding and property binding Example by defining proprty 
+  </h3>
+  <br>
+  <h3  [class.text-danger]="hasError">
+    Example of Condiditonaly applying html
+  </h3>
+  <br>
+  <h3  [ngClass]="messageClass">
+    Example of ngClass
+  </h3>
+  <br>
+  <button (click)="onClick()">Greet</button>
+  <br>
+  <br>
+  <input #message type="text"> 
+  <button (click)="getValueFromInputField(message.value)">Click</button>
+<br><br>
+<h2>Two way data binding</h2>
+  <input [(ngModel)]="name" type="text"> 
+  {{name}}
 
   `,
-  styleUrls: ['./test.component.css']
+  styles: [`
+        .text-success{
+          color:green;
+        }
+
+        .text-danger{
+          color:red;
+        }
+
+        .text-special{
+          font-style: italic;
+        }
+  `]
 })
 export class TestComponent implements OnInit {
 
   public username="wasif";
   public siteUrl=window.location.href;
   public isDisabled=true;
+  public  isSpecial=true;
+  public name="";
+
+  //Example of conditionaly applying css
+  public hasError=true;
+
+  //Example of class binding
+  public successClass="text-success"
+
+  //Example of ngClass directory
+  public messageClass={
+    "text-success" : !this.hasError,
+    "text-danger": this.hasError,
+    "text-specail": this.isSpecial,
+  }
 
   constructor() { }
 
@@ -52,5 +109,15 @@ export class TestComponent implements OnInit {
 
   greating(){
     return this.username;
+  }
+
+  //on click event example
+  onClick(){
+    console.log("Welcome to angular world")
+  }
+
+  //example of template reference varialbes
+  getValueFromInputField(value:any){
+    console.log(value);
   }
 }
